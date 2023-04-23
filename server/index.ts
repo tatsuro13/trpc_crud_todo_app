74df9f429196331e447591310027ea3693e7d50f
Tatsuro Osuka <oscarwild13@gmail.com>
2023-04-23 13:06:49 +0900

    Expressとtrpcサーバの設定

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { initTRPC } from '@trpc/server';
+import * as trcpExpress from '@trpc/server/adapters/express';
+
+
+const app = express();
+const PORT = 3000;
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+    hello: t.procedure.query(() => {
+        return 'Hello World!';
+    }),
+});
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+
+app.use('/trpc',trcpExpress.createExpressMiddleware({
+    router: appRouter,
+})
+);
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}!!`));
+
+export type AppRouter = typeof appRouter;
