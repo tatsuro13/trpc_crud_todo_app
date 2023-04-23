811ccdc00bb23a234531b1a4b784a33e206bdb73
Tatsuro Osuka <oscarwild13@gmail.com>
2023-04-23 15:32:48 +0900

    クライアントからのデータ取得

--- client/src/components/Test.tsx
+++ client/src/components/Test.tsx
@@ -2,15 +2,17 @@ import { trpc } from "../utils/trpc";
 
 const Test = () => {
 
+    const { data: todos } = trpc.todos.useQuery();
     const test = trpc.helloName.useQuery({name: 'Taro', age: 20});
 
     // const helloQuery = trpc.hello.useQuery();
     // console.log(helloQuery);
   return (
-      <>
-          <p>{test.data?.greeting}</p>
-          <p>{test.data?.age}</p>
-      </>
+      <ul>
+          {todos?.map((todo) => (
+              <li key={todo.id}>{todo.title}</li>
+            ))}
+      </ul>
   )
 }
 
